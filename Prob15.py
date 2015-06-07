9f56efdd07e4dd2a200f6252dde649c887d22018
KTJ10496 <KshitijSaraogi@gmail.com>
2015-06-07 20:35:50 +0530

    added Prob15.py

--- Prob15.py
+++ Prob15.py
@@ -1,6 +1,10 @@
-def counting(n):
-	if n == 2:
-		return 6
+def path(n):
+	if n == [0,0]:
+		return 1
+
+	else:
+		if n[0]>0:
+			path_count += path(n[0] - 1,)
 	count = 2
 	count *= counting(n-1)
 	return count
